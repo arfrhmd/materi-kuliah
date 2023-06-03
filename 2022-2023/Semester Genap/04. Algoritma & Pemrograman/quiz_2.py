ac30897f9fbf071868598f2f1bb59ba0b62150ad
Arif Rahmadi <41944717+arfrhmd@users.noreply.github.com>
2023-06-03 13:42:30 +0700

    Create quiz_2.py

---
+++ 2022-2023/Semester Genap/04. Algoritma & Pemrograman/quiz_2.py
@@ -0,0 +1,24 @@
+def hitung_pecahan(jumlah_uang):
+    uang_pecahan = [
+        50000, 20000, 10000,
+        5000, 2000, 1000,
+        500, 100
+    ]
+    hasil_pecahan = {}
+    
+    for pecahan in uang_pecahan:
+        count = 0
+        while jumlah_uang >= pecahan:
+            count += 1
+            jumlah_uang -= pecahan    
+        if count != 0:
+            hasil_pecahan[pecahan] = count
+    
+    return hasil_pecahan
+  
+  
+if __name__ == "__main__":
+    uang = int(input("Masukkan jumlah uang : "))
+    hasil_pecahan = hitung_pecahan(uang)
+    
+    print(hasil_pecahan)
